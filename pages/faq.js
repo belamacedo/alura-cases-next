e21d50ba2cd134691e185630f8602c45b259a73a
isabelamacedo <isabelacb.macedo@gmail.com>
2023-02-16 11:59:24 -0300

    feat: getServerSideoProps and getStaticProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,45 @@
 import Link from '../src/components/Link';
 
-export default function FAQPage() {
+// SSG - Static Site Generation
+// SSR - Server Side Rendering
+// ISG -  Incremental Static Generation
+
+// getServerSideProps: roda a cada acesso que você recebe. **Em modo dev sempre roda a cada acesso.
+// getStaticProps: roda SOMENTE em build time. **Em modo dev sempre roda cada acesso.
+
+export async function getStaticProps() {
+  const FAQ_API_URL =
+    'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+  const faq = await fetch(FAQ_API_URL)
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json();
+    })
+    .then((resposta) => {
+      return resposta;
+    });
+
+  return {
+    props: {
+      faq,
+    },
+  };
+}
+
+export default function FAQPage({ faq }) {
   return (
     <div>
       <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
       <Link href='/'>Ir para a home</Link>
+      <ul>
+        {faq.map(({ answer, question }) => (
+          <li key={question}>
+            <article>
+              <h2>{question}</h2>
+              <p>{answer}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
